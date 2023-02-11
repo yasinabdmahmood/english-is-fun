6ab06c8bba28be5ed41fbe8ac8a44eb83c5fa786
yasinabdmahmood <abd181103@gmail.com>
2023-02-12 00:35:51 +0300

    Modify index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import Head from "next/head";
 import ColoredText from "./coloredText";
 import { useState } from "react";
 import styles from "./index.module.css";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
   const [word, setWord] = useState('');
   const options = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
   const [selectedOption, setSelectedOption] = useState(options[0]);
   async function generateWord(event) {
+    setIsLoading(true);
     event.preventDefault();
     try {
       const response = await fetch("/api/getWord", {
@@ -26,10 +29,12 @@ export default function Home() {
       }
 
       setWord(data.result);
+      setIsLoading(false);
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+      setIsLoading(false);
     }
   }
   ////////////////////////////////////
@@ -58,6 +63,10 @@ export default function Home() {
     }
   }
 
+  const handleLevelChange = (e) => {
+
+  }
+
   return (
     <div>
       <Head>
@@ -68,13 +77,21 @@ export default function Home() {
       <main className={styles.main}>
         <img src="/english.png" style={{width: '100px', marginBottom: '2rem'}} />
         <ColoredText text={'English is fun'} />
-        <select value={selectedOption} onChange={e => setSelectedOption(e.target.value)}>
-      {options.map(option => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+        <div className={styles["levels-container"]}>
+          <h4>Select Fluency level</h4>
+          <ul className={styles["levels"]}>
+            {options.map(option => (
+            <li key={option}   >
+              <button
+              className= {option===selectedOption? styles['selected-level']: null}
+              onClick={()=>setSelectedOption(option)}>
+                {option}
+              </button>
+            </li>
+        ))}
+          </ul>
+        </div>
+    
         <form onSubmit={onSubmit}>
           <textarea
             name="description"
@@ -82,10 +99,13 @@ export default function Home() {
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Get graded" />
+          <button type="submit" className={styles["myButton"]}>
+          Get graded
+          </button>
         </form>
-        <button className="btn" onClick={generateWord}>Get word</button>
+        <button className={styles["myButton"]} onClick={generateWord}>Get word</button>
         <h3>{word}</h3>
+        {isLoading ? <LoadingSpinner /> : null}
         <div className={styles.result}>{result}</div>
       </main>
     </div>
