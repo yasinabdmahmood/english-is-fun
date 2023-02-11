d7e1a4d3be8d41c6cd224404f52533409a84d4af
yasinabdmahmood <abd181103@gmail.com>
2023-02-11 21:54:22 +0300

    Add api to get feedback

---
+++ pages/api/getFeedback.js
@@ -0,0 +1,51 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const word = req.body.word;
+  const description = req.body.description;
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(word,description),
+      temperature: 0.6,
+      max_tokens: 100,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(word,description) {
+return `A student in english test was asked to give desciption to word ${word} and 
+  he gave this description "${description}" grade his description from 1 to 10 based on 
+  how good of a description he gave, make sure to give a good description of the word as well 
+  when responding assume that you are directly giving the studen the give back by refering
+  to them as you instead of the student`;
+}
