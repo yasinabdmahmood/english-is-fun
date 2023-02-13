77872ffb0ac058f119c5b14c1eaf0769fc5d21ab
yasinabdmahmood <abd181103@gmail.com>
2023-02-13 19:03:29 +0300

    Add new component to handle colorful text

--- pages/coloredText.js
+++ pages/coloredText.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import { useRouter } from 'next/router';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const ColoredText = ({ text }) => {
+  const linearConversion = (x) => {
+    return (x - 375) * (3 - 1) / (1300 - 375) + 1.5;
+  }
+  const router = useRouter();
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    console.log(screenSize.width)
+    const bodyStyles = window.getComputedStyle(document.body);
+    setScreenSize({
+      width: parseInt(bodyStyles.width),
+      height: parseInt(bodyStyles.height)
+    });
+  }, [router]);
   return (
     <div>
       {text.split('').map((letter, index) => (
         <span 
           key={index}
           style={{ color: colors[index % colors.length], 
-          fontSize: '3rem',
+          fontSize: `${linearConversion(screenSize.width)}rem`,
           fontFamily: 'monospace',
           fontWeight: 'bold',
           }}>
