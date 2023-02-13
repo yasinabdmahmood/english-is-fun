2970b844d7ec8bb56ce4e7080260737fdf57ce77
yasinabdmahmood <abd181103@gmail.com>
2023-02-13 21:32:31 +0300

    Remove colored import statement

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import ColoredText from "./coloredText";
+
 import { useState } from "react";
 import styles from "./index.module.css";
 import LoadingSpinner from "./LoadingSpinner";
